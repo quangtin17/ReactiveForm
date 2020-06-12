89166f38e8fc1d239bde12e79670bbfd92a55306
quangtin17 <tin.truong@kyanon.digital>
2020-06-12 20:31:10 +0700

    create UI form

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCvgWjChp_-6fwhvBsD63Cm9vk6cxmakpo',
+    authDomain: 'reactive-form17.firebaseapp.com',
+    databaseURL: 'https://reactive-form17.firebaseio.com',
+    projectId: 'reactive-form17',
+    storageBucket: 'reactive-form17.appspot.com',
+    messagingSenderId: '550071174990',
+    appId: '1:550071174990:web:888f8ceb02e2d2e274001f',
+    measurementId: 'G-WHS1F0QM1F'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
